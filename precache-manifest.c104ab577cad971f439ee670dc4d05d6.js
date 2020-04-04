207b6c3f56af245a06994a1f0116aa5bf6216f01
pooja-prasannan <poojap.b95@gmail.com>
2020-04-04 16:55:39 +0530

    Updates

---
+++ precache-manifest.c104ab577cad971f439ee670dc4d05d6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cf56f50d3c6cc72e87023a41631158d",
+    "url": "/Assignment/index.html"
+  },
+  {
+    "revision": "f55a835afde5d7b0ad96",
+    "url": "/Assignment/static/js/2.4c5ae7e7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Assignment/static/js/2.4c5ae7e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc41747341d194b0d230",
+    "url": "/Assignment/static/js/main.9b116ef9.chunk.js"
+  },
+  {
+    "revision": "09589b56522d6873bbf2",
+    "url": "/Assignment/static/js/runtime-main.3862610c.js"
+  },
+  {
+    "revision": "d5f2a7f493e94ed267d41620eef143d9",
+    "url": "/Assignment/static/media/poster1.d5f2a7f4.jpg"
+  },
+  {
+    "revision": "81ccf144677486906fcf751450652895",
+    "url": "/Assignment/static/media/poster2.81ccf144.jpg"
+  },
+  {
+    "revision": "e242f0372e7bdbf2e48aa5afefc72b97",
+    "url": "/Assignment/static/media/poster3.e242f037.jpg"
+  },
+  {
+    "revision": "56f097245c605a5d312f103dedb21bcb",
+    "url": "/Assignment/static/media/poster4.56f09724.jpg"
+  },
+  {
+    "revision": "5ef7f79461c7a8c0a95652b813fb4722",
+    "url": "/Assignment/static/media/poster5.5ef7f794.jpg"
+  },
+  {
+    "revision": "164b05a3cd770650dd30bc71f5407e99",
+    "url": "/Assignment/static/media/poster6.164b05a3.jpg"
+  },
+  {
+    "revision": "23ac494a92660204d6b8acbba20aa0e9",
+    "url": "/Assignment/static/media/poster7.23ac494a.jpg"
+  },
+  {
+    "revision": "ef89f612f6e3d684892d6525b7957ce4",
+    "url": "/Assignment/static/media/poster8.ef89f612.jpg"
+  },
+  {
+    "revision": "65d6d5002a2f98860f29d3c61599c5ac",
+    "url": "/Assignment/static/media/poster9.65d6d500.jpg"
+  }
+]);
